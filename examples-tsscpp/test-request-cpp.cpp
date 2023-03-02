6ce885a2109f25792fdd5311f0e66bdadffee09c
GustaMonteiro <gcvm6070@gmail.com>
2023-03-02 16:21:30 -0300

    Change request parameters and add response handling

--- examples-tsscpp/test-request-cpp.cpp
+++ examples-tsscpp/test-request-cpp.cpp
@@ -16,19 +16,17 @@
 int main(int, char**)
 {
     try {
-
-
         Json::FastWriter writer;
         Json::Value root;
         Json::Value akParameters;
         root["ID"] = "2";
-        root["TPMVersion"] = "2";
-        root["EKpub"] = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2FuIS+QMG8dyOTQ9kadzInTymMnmafXNAPAq37WjpH/essTDn+k4vTfW/QMs+8xs+2pe3GMgmKWBybcmUzAL/NueCRxCF4LuJHYyqYTjxK+zxRkZT6ZqMopyChTZ0qSp/EYYdd+9YB56vryQ2iBnJbxg7PSjMl6kAcvzjYK85U/sVAS6V+k+DruMu2gdB5rQgxg470tuPRN8tE+N6XHCUPPf5EqF9eLmBObvRuwr+FUKcEMqjOkBJPGJFoMypMs5vJzj40gtm14lNDn3B3rzsZ4Ww2DSHF1HTLRnKBbkM24gE2x9SFpK/yGbIH9H0xp+FbGvFg3/oZ0APcpH7sJz6wIDAQAB";
-        akParameters["Public"] = "AAEACwAFBHIAIJ3/y/NsODrmmfuYaNxty4nXFTiEvigDkiwSQVi/rSKuABAAFAAECAAAAAAAAQChh2H07bgAmP8JGxdqG0qQxilZu3m0HaHOv9VmNznb1phyZhMkwPUExPwMaaEEIdRpshtc/tRdKXElp3omSCCSNaE6JbjNLvLxiGKVotSB6zAB6ZvL4WuDJjtQqD1NKzpvOtpLYmqg1MwV3PYEYDLpU/MjEYA9abNXjnuqUtM0p6lUnsBRKRKxnUgwsMTpzF8mDjPzFeEwVmdSQwsFOdqTxZHAisZEicCVO3kpU13yybR7wyUzVPoGoIM0GSMezQsI9jCYlqzmCf6T1mTmwBmzXN8Pbz2S56bTxjiCnM13uIljuP90TJPSZuq/YCuNGFffF3YZHfwwLpCM8uy8Lxpn";
-        akParameters["UseTCSDActivationFormat"] = "false";
-        akParameters["CreateData"] = "AAAAAAAg47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFUBAAsAIgALJhjDCmNrglM43Tq2ygzrMYalA2AQ4c54TQkzJnQKpkQAIgALOxuOMGNb/ZGcB6mDOBv63kDHssbCY5HrDWqlw6E7FtwAAA==";
-        akParameters["CreateAttestation"] = "/1RDR4AaACIAC/6XT2Vmijo9KCOUp2h3xNOcmwWmdWwB3UE2Dj9Cj8IoAAAAAAACj61kUEwQvHAooHuHAWaOtmbh1ELoACIACwyF5bHZ05fFF0VYOA4fFX9xXX92ajKvtsTxjaXxRBJwACBZFibRNyqUH1y3B5tGxp/zQhVuu1o8dR5oEXkV3r5vpA==";
-        akParameters["CreateSignature"] = "ABQABAEAcG48uD9hinoEn1plN/VO2nRqxM4nzc+dLp7KOuitMbfd/8JaluBJqy/6AyKSqKdcAwzGFs/r8UrLoiC8P0cVTWoxXWl4VA5R4fgaK+lbb4OLOUQ2HMJpMgUS0QsveSc6yAQ4QUe3trz1BwuQuhZMiRXNwtp73ffzY1jHos+ASVA2wZJRXjuEiMTdCQwJ6oZDSPXtnAa2aq/1LKPJfQmk6buYMhdFTM2VuqAVIy/yW8HaBsAqp8gwsqnkitA/fFSWe2ehy4YLzsFMdesrovHr/Kc1wD773DYRglCujk13lsFEtTMb6QPDtITqoL9Rr3rR/4svPTltP2uTkZnqbeRBeg==";
+        root["TPMVersion"] = 2;
+        root["EKpub"] = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAthSyFeVj8dWdDmJBfYP8ElsPfSna5zR7nDADTuqREKL5KTUAIZmw7JGsC19YP1K/m50M1NMTAyyIeqctTy1vn94vehBrSEotbIVyj/Z45rVDhCcx0Jn4EkjopaoM7xMgt9R/NOCyX8gXLU+F6Afr6lc7R9ob3MIOR+1z7QvnXSN+hMvh9m5dBBYTI+UsJ5w1+z+X69VQu0fGe4c2yL6Vw6SLY6/2lxGKpGTWdoFm+Nn/XojGqTOhPZiQ99T12InlWLkppH5bMVzLUIshaRJPN8rOnMwdkVuChOsNJ+eHExX8TcivIhBMHG1yTRABKEC+serDK/p4f047+XttwDcNNwIDAQAB";
+        akParameters["Public"] = "AAEACwAFBHIAIJ3/y/NsODrmmfuYaNxty4nXFTiEvigDkiwSQVi/rSKuABAAFAAECAAAAAAAAQDrrr22jWa5ySy/rno3WMLG05KewCGj4jN0Y8pK2+83V4qiqpOBSIKmlsnKneUM2uFaWhhGlapvXWAD3KGVOZ9NYU2q2+uHDXV6IkPVgVo/S4qN9+2+l/erp7FwDrRLgEomoIzNdgWnVYn+NVQxJXTHTvQrw2fVeYSpsu9+feQ3oaYTCDf+RW7g7x0E84rdXig86s5cs6xtXijEUZtm4dhZBaw1lP0r0SLRD2MtQep5RpDlLOHaCp9ghneM4XK6I/oRonRMo1jvyYWyg8vAmmndCrH4x6TViWiVdUKqo7+ptS/r6mqUKEShJ9umrO2GIBIS3Zyx49PcKkKRxfsmWuUJ";
+        akParameters["UseTCSDActivationFormat"] = false;
+        akParameters["CreateData"] = "AAAAAAAg47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFUBAAsAIgALOyDqlR1A7yIRtJUe4a8rxHtxVJItEjRTilRv/M5DzkYAIgAL2wSQnCKgLL1TGKUreXra2BFHEkr5koxLbxud+2xQuvsAAA==";
+        akParameters["CreateAttestation"] = "/1RDR4AaACIAC0/Ii5jg4zsvxVlMA2EhIMgtmeacFlfseS/pO+dCwez7AAAAAAACOnBH7HHpWtdLb0IaAb6Z3ehFCJZRACIAC4sH8uLNoGTQavVwmFQjS1dfff/D13bXfJef2B8BmW0mACB9xuiDscZkcaz2KUoBUhQBNi4ijSBI04jKkUlZNyd9Aw==";
+        akParameters["CreateSignature"] = "ABQABAEAgCDlvMh6fzsKbTE97gWWZ3VIyflMSkbzyADlu30lXqfzYfi/nDji5XSFwW2lDUXB+3L4sshhfFtjxRdPpEcGUEgp/3oyI+WkraNaT+p7Xv7TIcGWBVqEadB4K0f+DxLnHGkc91PeugBppEZ6DNpL73uQ6+EeNSVJOOTNwpduInqeE30DzHwwCa6Ist8gU4RIfQHzWsHqKl/d5rXp4Rm5wAB6s54k9rxi3z0s3x+cEb9xuL4KyoXK2mPJgFACUgo0cDwRRtzB0gY8M+W73tXdnkA5MdJGHsOfipYzqNiVQZoJxJR0wk0/BORQvYU7d/LI3BXc1vwWSobrbqLgZEt/HA==";
 
         root["AK"] = akParameters;
 
@@ -48,10 +46,10 @@ int main(int, char**)
         // Setting the URL to retrive.
         request.setOpt(new curlpp::options::Url("http://localhost:8080/initialChallenge"));
 
-        std::list<std::string> header;
-        header.push_back("Content-Type: application/octet-stream");
+        //std::list<std::string> header;
+        //header.push_back("Content-Type: application/octet-stream");
 
-        request.setOpt(new curlpp::options::HttpHeader(header));
+        //request.setOpt(new curlpp::options::HttpHeader(header));
 
         request.setOpt(new curlpp::options::PostFields(sendData));
         request.setOpt(new curlpp::options::PostFieldSize(sendData.size()));
@@ -60,6 +58,15 @@ int main(int, char**)
 
         std::cout << "Response from server:" << std::endl;
         std::cout << response.str() << std::endl;
+
+        Json::Reader reader;
+
+        Json::Value responseJson;
+
+        reader.parse(response.str(), responseJson);
+
+        std::cout << "Credential: " << responseJson["Encrypted Credential"]["Credential"].asString() << std::endl;
+        std::cout << "Secret: " << responseJson["Encrypted Credential"]["Secret"].asString() << std::endl;
 
         return EXIT_SUCCESS;
     }
@@ -70,5 +77,5 @@ int main(int, char**)
         std::cout << e.what() << std::endl;
     }
 
-	return 0;
+    return 0;
 }
